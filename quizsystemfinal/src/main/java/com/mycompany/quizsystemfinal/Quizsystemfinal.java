8c7c900933177952ed5a66832a9168c3585dbfed
ishy1 <ishy1@10.242.165.72>
2025-01-07 13:57:11 -0500

    Merge origin/master
    # Conflicts:
    #	src/linkedliststarter/LinkedListStarter.java

---
+++ quizsystemfinal/src/main/java/com/mycompany/quizsystemfinal/Quizsystemfinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quizsystemfinal;
+
+/**
+ *
+ * @author ishy1
+ */
+public class Quizsystemfinal {
+
+    public static void main(String[] args) {
+        System.out.println("Heasdasdllo World!");
+    }
+}
